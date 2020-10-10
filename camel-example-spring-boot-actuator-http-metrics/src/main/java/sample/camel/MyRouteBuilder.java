6aaf4aed1b7037326df33ab1fbc2da2c0eefcf57
JoshiRaez <joseraezrodriguez@gmail.com>
2020-10-10 12:52:57 +0200

    Built routes for mappings and metrics

---
+++ camel-example-spring-boot-actuator-http-metrics/src/main/java/sample/camel/MyRouteBuilder.java
@@ -0,0 +1,53 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package sample.camel;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.rest.RestBindingMode;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyRouteBuilder extends RouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+
+        // First, we have to configure our jetty component, which will be the product
+        // in charge of querying the REST endpoints from actuator
+        restConfiguration().component("jetty")
+                .host("0.0.0.0")
+                .port(8080)
+                .bindingMode(RestBindingMode.json);
+
+        // We will first show the routes we have exposed.
+        from("timer:queryTimer?repeatCount=1")
+                .to("rest:get:/actuator/mappings")
+                .unmarshal()
+                .json()
+                .to("log:INFO");
+
+        // Then, we will be querying the cpu consumption periodically. For more options, you can check
+        // https://docs.spring.io/spring-boot/docs/current/reference/html/production-ready-features.html#production-ready-metrics-endpoint
+        from("timer:metricsTimer?period={{metricsPeriod}}")
+                .to("rest:get:/actuator/metrics/system.cpu.usage")
+                .unmarshal().json()
+                .to("log:INFO");
+
+        // Left to oversee how to stop routes
+    }
+}
