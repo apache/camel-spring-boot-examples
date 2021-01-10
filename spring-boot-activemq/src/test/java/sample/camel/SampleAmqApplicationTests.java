aba7f15bf8df526dfcae3b7c6c557126751e63b1
metters <matthias.streidel@gmail.com>
2021-01-10 13:08:14 +0100

    remove `camel-example-` prefix from `camel-example-spring-boot-activemq`

--- spring-boot-activemq/src/test/java/sample/camel/SampleAmqApplicationTests.java
+++ spring-boot-activemq/src/test/java/sample/camel/SampleAmqApplicationTests.java
@@ -24,6 +24,7 @@ import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.camel.test.spring.junit5.CamelSpringBootTest;
 import org.junit.jupiter.api.Disabled;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
