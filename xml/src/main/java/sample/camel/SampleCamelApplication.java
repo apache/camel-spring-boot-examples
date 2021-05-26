09fb72cebf02bfca109a13d31fe368eee92d8e29
Claus Ibsen <claus.ibsen@gmail.com>
2021-05-26 09:18:08 +0200

    xml dsl example

--- xml/src/main/java/sample/camel/SampleCamelApplication.java
+++ xml/src/main/java/sample/camel/SampleCamelApplication.java
@@ -18,15 +18,12 @@ package sample.camel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ImportResource;
 
 //CHECKSTYLE:OFF
 /**
  * A sample Spring Boot application that starts the Camel routes.
  */
 @SpringBootApplication
-// load the spring xml file from classpath
-@ImportResource("classpath:my-camel.xml")
 public class SampleCamelApplication {
 
     /**
