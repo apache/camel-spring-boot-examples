f0935967ca344678cc31258574a77cb9727c375b
Claus Ibsen <claus.ibsen@gmail.com>
2022-06-22 14:48:21 +0200

    CAMEL-18149: camel-spring-boot - Add camel developer console as actuator that can be enabled.

--- spring-boot/src/main/java/sample/camel/MyCamelRouter.java
+++ spring-boot/src/main/java/sample/camel/MyCamelRouter.java
@@ -23,7 +23,7 @@ import org.springframework.stereotype.Component;
 /**
  * A simple Camel route that triggers from a timer and calls a bean and prints to system out.
  * <p/>
- * Use <tt>@Component</tt> to make Camel auto detect this route when starting.
+ * Use <tt>@Component</tt> to make Camel auto-detect this route when starting.
  */
 @Component
 public class MyCamelRouter extends RouteBuilder {
