f1a12895b519166f7e4685efd459542a4dcc6037
kariuwu <xduise00@vutbr.cz>
2022-08-24 15:58:29 +0200

    update infinispan refactor

--- infinispan/src/main/java/org/apache/camel/example/springboot/infinispan/Application.java
+++ infinispan/src/main/java/org/apache/camel/example/springboot/infinispan/Application.java
@@ -17,28 +17,15 @@
 package org.apache.camel.example.springboot.infinispan;
 
 import java.util.Properties;
-import java.util.function.Consumer;
-
-import com.github.dockerjava.api.command.CreateContainerCmd;
-import com.github.dockerjava.api.model.ExposedPort;
-import com.github.dockerjava.api.model.PortBinding;
-import com.github.dockerjava.api.model.Ports;
 
 import org.apache.camel.component.infinispan.remote.InfinispanRemoteComponent;
 import org.apache.camel.component.infinispan.remote.InfinispanRemoteConfiguration;
 import org.infinispan.client.hotrod.RemoteCacheManager;
 import org.infinispan.client.hotrod.configuration.ConfigurationBuilder;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.testcontainers.containers.BindMode;
-import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.containers.output.OutputFrame;
-import org.testcontainers.containers.output.Slf4jLogConsumer;
-import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.shaded.org.apache.commons.lang.SystemUtils;
 
 
@@ -47,18 +34,14 @@ import org.testcontainers.shaded.org.apache.commons.lang.SystemUtils;
 public class Application {
     
     
-    private static final Logger LOG = LoggerFactory.getLogger(Application.class);
+   /*private static final Logger LOG = LoggerFactory.getLogger(Application.class);
     
     private static final String CONTAINER_IMAGE = "quay.io/infinispan/server:13.0.5.Final-1";
-    private static final String CONTAINER_NAME = "infinispan";
+    private static final String CONTAINER_NAME = "infinispan"; */
     private static final String DEFAULT_USERNAME = "admin";
     private static final String DEFAULT_PASSWORD = "password";
-    private static final int CONTAINER_PORT = 11222;
+    private static final int CONTAINER_PORT = 11222; 
     
-    private GenericContainer<?> container;
-    /**
-     * Main method to start the application.
-     */
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
@@ -67,28 +50,6 @@ public class Application {
     
     @Configuration
     public class InfinispanConfiguration {
-
-        private void initContainer() {
-            LOG.info("start infinispan docker container");
-            Consumer<CreateContainerCmd> cmd = e -> {
-                e.getHostConfig().withPortBindings(new PortBinding(Ports.Binding.bindPort(CONTAINER_PORT),
-                                                   new ExposedPort(CONTAINER_PORT)));
-
-            };
-            final Logger containerLog = LoggerFactory.getLogger("container." + CONTAINER_NAME);
-            final Consumer<OutputFrame> logConsumer = new Slf4jLogConsumer(containerLog);
-
-            container = new GenericContainer<>(CONTAINER_IMAGE).withNetworkAliases(CONTAINER_NAME)
-                .withEnv("USER", DEFAULT_USERNAME).withEnv("PASS", DEFAULT_PASSWORD)
-                .withLogConsumer(logConsumer)
-                .withClasspathResourceMapping("infinispan.xml", "/user-config/infinispan.xml",
-                                              BindMode.READ_ONLY)
-                .withCommand("-c", "/user-config/infinispan.xml").withExposedPorts(CONTAINER_PORT)
-                .withCreateContainerCmdModifier(cmd).waitingFor(Wait.forListeningPort())
-                .waitingFor(Wait.forLogMessage(".*Infinispan.*Server.*started.*", 1));
-            container.start();
-
-        }
 
         protected ConfigurationBuilder getConfiguration() {
             ConfigurationBuilder clientBuilder = new ConfigurationBuilder();
@@ -112,7 +73,6 @@ public class Application {
 
         @Bean(name = "infinispanRemoteComponent")
         public InfinispanRemoteComponent infinispanRemoteComponent() {
-            initContainer();
             InfinispanRemoteConfiguration infinispanRemoteConfiguration = new InfinispanRemoteConfiguration();
             infinispanRemoteConfiguration.setHosts("localhost" + ":" + CONTAINER_PORT);
             infinispanRemoteConfiguration.setUsername(DEFAULT_USERNAME);
