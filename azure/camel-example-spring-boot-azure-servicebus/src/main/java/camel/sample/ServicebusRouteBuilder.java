ecba91bc71b04fec095f8a3ed04bbd58df536584
hui <hui1110rui@163.com>
2022-09-19 18:36:18 +0800

    add azure samples

---
+++ azure/camel-example-spring-boot-azure-servicebus/src/main/java/camel/sample/ServicebusRouteBuilder.java
@@ -0,0 +1,44 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package camel.sample;
+
+import org.apache.camel.builder.endpoint.EndpointRouteBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ServicebusRouteBuilder extends EndpointRouteBuilder {
+
+    @Autowired
+    private MessageReceiver receiver;
+
+    @Value("${queueName}")
+    private String queueName;
+
+    @Override
+    public void configure() {
+        from(timer("tick").period(1000))
+            .setBody(constant("Service Bus Test"))
+            .to(azureServicebus(queueName));
+
+        from(azureServicebus(queueName))
+            .bean(receiver)
+            .log("The content is ${body}");
+    }
+
+}
