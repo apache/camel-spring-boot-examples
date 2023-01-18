b983bab9d2a94d091ac945a71ec127ba684225a8
Andrea Cosentino <ancosen@gmail.com>
2023-01-18 14:30:08 +0100

    Move to 4.0.0-SNAPSHOT
    Signed-off-by: Andrea Cosentino <ancosen@gmail.com>

--- azure/camel-example-spring-boot-azure-servicebus-jms/src/main/java/sample/camel/SampleCamelServicebusJmsApplication.java
+++ azure/camel-example-spring-boot-azure-servicebus-jms/src/main/java/sample/camel/SampleCamelServicebusJmsApplication.java
@@ -23,7 +23,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import javax.jms.ConnectionFactory;
+import jakarta.jms.ConnectionFactory;
 
 @SpringBootApplication
 public class SampleCamelServicebusJmsApplication {
