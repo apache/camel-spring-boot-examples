b99019abb981577423ce6674c66f22ca7b044462
Marcin Grzejszczak <mgrzejszczak@vmware.com>
2023-03-27 17:01:07 +0200

    Added support for Micrometer Observation

---
+++ observation/client/src/main/java/sample/camel/ClientApplication.java
@@ -0,0 +1,49 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package sample.camel;
+
+import io.micrometer.core.instrument.MeterRegistry;
+import org.apache.camel.observation.starter.CamelObservation;
+import sample.observation.logging.LoggingSpanHandler;
+import sample.observation.logging.MetricsPrinter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@CamelObservation
+public class ClientApplication {
+
+	/**
+	 * A main method to start this application.
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(ClientApplication.class, args);
+	}
+
+	@Bean
+	LoggingSpanHandler loggingSpanReporter() {
+		return new LoggingSpanHandler();
+	}
+
+	@Bean
+	MetricsPrinter metricsPrinter(MeterRegistry meterRegistry) {
+		return new MetricsPrinter(meterRegistry);
+	}
+
+}
