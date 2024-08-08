7e675c8616c68035359d45f515aacc664283fe91
Rinaldo Pitzer Júnior <rinaldopjr@redhat.com>
2024-08-08 12:24:45 -0300

    add tomcat-jdbc example

---
+++ tomcat-jdbc/src/main/java/sample/camel/MyCamelApplication.java
@@ -0,0 +1,50 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package sample.camel;
+
+import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Configuration;
+
+//CHECKSTYLE:OFF
+/**
+ * A sample Spring Boot application that starts the Camel routes.
+ *
+ * Having both the {@link #configure} and {@link #main} methods call the same builder ensures the configuration
+ * will be applied either by starting the application as a .war or by running as an executable.
+ */
+@SpringBootApplication
+public class MyCamelApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return customizerBuilder(builder);
+    }
+
+    public static void main(String[] args) {
+        customizerBuilder(new SpringApplicationBuilder()).run(args);
+    }
+
+    private static SpringApplicationBuilder customizerBuilder(SpringApplicationBuilder builder) {
+        return builder.sources(MyCamelApplication.class);
+    }
+
+}
+//CHECKSTYLE:ON
