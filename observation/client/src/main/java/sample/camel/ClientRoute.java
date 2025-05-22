558dbfe6ca16c24bc8c78d10784a2082e359bc28
Claudio Miranda <claudio@claudius.com.br>
2025-05-22 12:34:56 +0100

    observation: replaced jetty/undertow for platform-http/http components as the former were deprecated in csb Containers are an integral part of the example Updated configuration, micrometer version

--- observation/client/src/main/java/sample/camel/ClientRoute.java
+++ observation/client/src/main/java/sample/camel/ClientRoute.java
@@ -26,7 +26,7 @@ public class ClientRoute extends RouteBuilder {
 	@Override
 	public void configure() {
 		// you can configure the route rule with Java DSL here
-		from("timer:trigger?exchangePattern=InOut&period=30000").streamCaching()
+		from("timer:trigger?exchangePattern=InOut&period=30000").streamCache("true")
 				.bean("counterBean")
 				.log("Client request: ${body}")
 				.to("http://localhost:6060/service1")
